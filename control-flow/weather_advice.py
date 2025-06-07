04472d8505abab979d99d1be39ab19c90fa0ef73
Denis Kirimi <ownerkirimi@gmail.com>
2025-06-08 02:21:00 +0300

    if, elif, else in weather check

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,20 +1,12 @@
-#weather chek
+#weather check 
 
-while True:
-    weather=input("What's the weather like today? (sunny/rainy/cold or 'exit' to quit):").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
-    if weather== "sunny":
-        print ("Wear a t-shirt and sunglasses.\n")
-
-    elif weather== "rainy":
-        print("Don't forget your umbrella and a raincoat.\n")
-
-    elif weather== "cold":
-        print("Make sure to wear a warm coat and a scarf.\n")
-
-    elif weather == "exit":
-        print("Goodbye!")
-        break
-
-    else:
-        print("Sorry, I don't have recommendations for this weather.\n")
+if weather == "sunny":
+  print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+  print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+  print("Make sure to wear a warm coat and a scarf.")
+else:
+  print("Sorry, I don't have recommendations for this weather.")
